62b07f2ffe05887be40355db5f20de7c6a8ffbd3
Therakar <matteo.pau200@gmail.com>
2022-11-02 16:38:28 +0100

    fa in modo che a ogni elemento cliccato venga applicata la classe .selected grazie alla parola chiave this

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 "use strict"
 
-const boardContainer = document.querySelector(".boardContainer"); 
+const boardContainer = document.querySelector(".boardContainer");
 
 for (let i = 1; i <= 100; i++){ //ciclo for che va da 1 a 100 
     const boardCell = document.boardCell = document.createElement("div"); //crea l'elemento div
     boardCell.innerHTML = i; // inserisce un valore in ogniuno dei div
     boardCell.classList.add("boardNumber"); // assegna ai div la classe .boardNumber
     boardCell.addEventListener("click", function(){ //aggiunge un event listener a tutti gli elementi boardCell
-        alert("ELEMENTO SELEZIONATO!");
+        this.classList.add("selected");
+        alert(`HAI SELEZIONATO L'ELEMENTO:  ${i}`);
     });
     boardContainer.append(boardCell); //aggiunge gli elementi nell'html
 }
