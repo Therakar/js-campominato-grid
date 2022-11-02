7b9ee7f8f9f0a6371a31bd080759b0ce869c5960
Therakar <matteo.pau200@gmail.com>
2022-11-02 16:29:47 +0100

    aggiungo un event listener agli elemnti creati con il ciclo for

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 "use strict"
 
-const boardContainer = document.querySelector(".boardContainer");
+const boardContainer = document.querySelector(".boardContainer"); 
 
-for (let i = 1; i <= 100; i++){
-    const boardCell = document.boardCell = document.createElement("div");
-    boardCell.innerHTML = i;
-    boardCell.classList.add("boardNumber");
-    boardContainer.append(boardCell);
+for (let i = 1; i <= 100; i++){ //ciclo for che va da 1 a 100 
+    const boardCell = document.boardCell = document.createElement("div"); //crea l'elemento div
+    boardCell.innerHTML = i; // inserisce un valore in ogniuno dei div
+    boardCell.classList.add("boardNumber"); // assegna ai div la classe .boardNumber
+    boardCell.addEventListener("click", function(){ //aggiunge un event listener a tutti gli elementi boardCell
+        alert("ELEMENTO SELEZIONATO!");
+    });
+    boardContainer.append(boardCell); //aggiunge gli elementi nell'html
 }
