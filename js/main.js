d63490eb742a1fcd03be547839f89584d41843d2
Therakar <matteo.pau200@gmail.com>
2022-11-02 16:22:58 +0100

    crea un ciclo for che va da 1 a 100 che utilizzo pre creare le tessere da mettere all'interno del tabellone

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict"
+
+const boardContainer = document.querySelector(".boardContainer");
+
+for (let i = 1; i <= 100; i++){
+    const boardCell = document.boardCell = document.createElement("div");
+    boardCell.innerHTML = i;
+    boardCell.classList.add("boardNumber");
+    boardContainer.append(boardCell);
+}
